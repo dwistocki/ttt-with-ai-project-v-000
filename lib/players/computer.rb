71587aabbf31f3da057299789388ba5bc6d2bda7
David Wistocki <david@davidwistocki.com>
2017-03-20 01:32:36 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,5 +2,10 @@ module Players
   class Computer < Player
     def initialize
     end
+
+    def move(board)
+      puts "What's your move (use a number 1-9)?"
+      gets.strip
+    end
   end
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,8 +1,8 @@
 module Players
   class Human < Player
     def move(board)
-      mover = gets.strip
-      return mover
+      puts "What's your move (use a number 1-9)?"
+      gets.strip
     end
   end
 end
